26cb98d5510c74d8ea4b63c422347922a8d21dbb
Hans Cantanhede <hansnewton@gmail.com>
2020-08-09 00:09:22 -0300

    finalizando layout give-classes, criando componentes Input, Select e Textarea reutilizaveis

--- proffy-web/src/pages/TeacherList/index.tsx
+++ proffy-web/src/pages/TeacherList/index.tsx
@@ -4,26 +4,45 @@ import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 function TeacherList() {
   return (
     <div id='page-teacher-list' className="container">
       <PageHeader title="Estes são os proffs disponíveis">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Horário</label>
-            <input type="text" id="time"/>
-          </div>
+        <Select 
+              name='subject' 
+              label='Matéria' 
+              options={[ 
+                {value: 'Artes', label: 'Artes'},
+                {value: 'Biologia', label: 'Biologia'},
+                {value: 'Ciências', label: 'Ciências'},
+                {value: 'Educação Física', label: 'Educação Física'},
+                {value: 'História', label: 'História'},
+                {value: 'Geografia', label: 'Geografia'},
+                {value: 'Matemática', label: 'Matemática'},
+                {value: 'Química', label: 'Química'},
+                {value: 'Português', label: 'Português'}
+               ]}
+              />
+          <Select 
+              name='week_day' 
+              label='Dia da Semana' 
+              options={[ 
+                {value: '0', label: 'Domingo'},
+                {value: '1', label: 'Segunda-feira'},
+                {value: '2', label: 'Terça-feira'},
+                {value: '3', label: 'Quarta-feira'},
+                {value: '4', label: 'Quinta-feira'},
+                {value: '5', label: 'Sexta-feira'},
+                {value: '6', label: 'Sábado'},
+               ]}
+              />
+          <Input type="time" name="time" label="Hora"/>
+          
         </form>
       </PageHeader>
 
